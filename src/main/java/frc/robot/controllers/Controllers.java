f5bc1b9a2faef8364e8dfc9368cbf2545069501d
Henry <rhinosaregreat123@gmail.com>
2026-09-25 13:20:03 -0400

    added method to check all ports for controllers; NOT USED YET!

--- src/main/java/frc/robot/controllers/Controllers.java
+++ src/main/java/frc/robot/controllers/Controllers.java
@@ -1,5 +1,26 @@
 package frc.robot.controllers;
 
+import edu.wpi.first.wpilibj.DriverStation;
+
 public class Controllers {
-    
+    public Controllers(){}
+
+    private static final int MAX_DRIVERSTATION_PORTS = DriverStation.kJoystickPorts;
+
+    public static DriveController driveController;
+
+    public static void updateActiveControllers(){
+        boolean foundDriveController = false;
+
+        driveController = new DriveController(-1);
+
+        for (int port = 0; port < MAX_DRIVERSTATION_PORTS; port++) {
+            if (DriverStation.isJoystickConnected(port)) {
+                if (!foundDriveController && DriverStation.getJoystickIsXbox(port)) {
+                    foundDriveController = true;
+                    driveController = new DriveController(port);
+                }
+            }
+        }
+    }
 }
